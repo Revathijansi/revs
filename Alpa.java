9f18e2f5c0f9c410612be1ead744d00bbba2ff9b
Revathijansi <crevathipsr@gmail.com>
2016-07-21 17:07:03 +0530

    Update and rename Positive.java to Alpa.java

---
+++ Alpa.java
@@ -0,0 +1,17 @@
+class Alpha
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+char n=s.next();
+switch(n)
+{
+case a: System.out.printtln("is alphabet");
+case e: System.out.printtln("is alphabet");
+case i: System.out.printtln("is alphabet");
+case o: System.out.printtln("is alphabet");
+case u: System.out.printtln("is alphabet");
+default:System.out.println("is not alphabets");
+}
+}
+}
